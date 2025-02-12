c187855f91554cfd675b52820f9eee2eff3c6dde
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-02-12 10:27:10 +0530

    initial commit

---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Harsh Singh</h1>
+      <p>Contact: harsh.singh@example.com</p>
+    </div>
+  );
+};
+
+export default Home;
